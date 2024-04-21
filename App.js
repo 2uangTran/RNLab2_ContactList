b66d1a61644f120822e99ebf3d40c191e27c6111
tigon281 <it.minhquangpham@gmail.com>
2024-04-21 13:31:58 +0700

    UpLoad

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
+import MyStack from './navigation/stack_navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import Favorites from './screens/Favorites';
+import MyTabs from './navigation/bottom_navigation';
+import TabNavigator from './navigation/stack_navigation';
+import { PaperProvider, Provider } from 'react-native-paper';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
@@ -14,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  
 });
